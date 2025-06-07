fec9cfd4b097b42dd140255caab11fcb95c6eb5f
netcoffeecat <62831356+netcoffeecat@users.noreply.github.com>
2025-06-07 11:14:19 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "netcoffeecat"         # 用户名
+UUID = "1cd92d6f-c8e5-4e5b-932e-234335fc1353"                     # UUID，留空则自动生成
+PORT = 13980                   # Vmess端口，留空或0则自动生成
+DOMAIN = "agsb.tangning.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYmI0YzgxZjY0MjBkMzk1ZDlhNzJkYjM4ZjQyYjA5OTciLCJ0IjoiN2FiMzA1MTgtOGI4Ny00NDA3LTlhMDktYzE1ODYyMmZlMDIwIiwicyI6Ik56TmtPV0ZpTUdRdE9EZGlPQzAwWXpNNUxUbGxZMll0TTJFd1lUTXhPR1V5WXpabSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
